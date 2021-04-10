eb7d8517569407265f4182e0c067e27db802339c
Kavi Gupta <kavi@berkeley.edu>
2021-04-10 12:49:57 -0700

    use get image

--- generate_examples.py
+++ generate_examples.py
@@ -3,14 +3,11 @@ import tqdm
 
 from mapmaker.model import Model
 from mapmaker.data import all_data
+from mapmaker.generate_image import get_model, get_image
 
-model = Model(all_data(), alpha=0.1)
+model = get_model()
 model.sample("2020 Actual", seed=None, path="images/2020_actual.svg")
 model.sample("2020 Pred", seed=None, path="images/2020_pred.svg", correct=False)
 model.unbias_predictor()
 for i in tqdm.trange(1, 11):
-    model.sample(
-        f"2024 speculative {i}",
-        seed=i,
-        path=f"images/2024_speculative_{i}.svg",
-    )
+    get_image(i, i)
