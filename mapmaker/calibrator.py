7a074d937d1d6e87e2b81cd29156254ef564ad58
Kavi Gupta <kavi@berkeley.edu>
2021-04-23 18:58:01 -0700

    Fix parameters and have somewhat higher resolution in selecting them

--- mapmaker/calibrator.py
+++ mapmaker/calibrator.py
@@ -12,27 +12,26 @@ def bias(preds):
 
 
 def unbias_predictor(model, *, for_year, target_pv_spread_90=17.5e-2):
-    alphas = np.arange(0.05, 0.25, 0.05)
-    models = [model.with_alpha(alpha) for alpha in alphas]
-    preds_each = [
-        model.family_of_predictions(year=for_year) for model in tqdm.tqdm(models)
-    ]
-    pv_spread_90 = np.array(
-        [np.percentile(pv, 95) - np.percentile(pv, 5) for _, pv in preds_each]
-    )
-    print("Alpha values:", alphas)
-    print("Spreads:", pv_spread_90 * 100)
-    idx = np.argmin(np.abs(pv_spread_90 - target_pv_spread_90))
-    print("Optimal alpha:", alphas[idx])
-    print("Optimal spread:", pv_spread_90[idx])
-    model = models[idx]
-    state_preds, _ = preds_each[idx]
+    low_alpha, high_alpha = 0, 1
+    while True:
+        mid_alpha = (low_alpha + high_alpha) / 2
+        model = model.with_alpha(mid_alpha)
+        state_preds, pv = model.family_of_predictions(year=for_year)
+        pv_spread_90 = np.percentile(pv, 95) - np.percentile(pv, 5)
+        print(f"Alpha: {mid_alpha:.4f}")
+        print(f"Spread: {pv_spread_90:.2%}")
+        if abs(pv_spread_90 - target_pv_spread_90) < 0.5e-2:
+            break
+        if pv_spread_90 > target_pv_spread_90:
+            high_alpha = mid_alpha
+        else:
+            low_alpha = mid_alpha
 
     print(
         f"Without correction, democrats win the EC {bias(state_preds):.2%} of the time"
     )
 
-    bias_values = np.arange(-0.02, -0.005, 0.001)
+    bias_values = np.arange(-0.1, 0.1, 0.01e-2)
     biases = np.array([bias(state_preds + x) for x in tqdm.tqdm(bias_values)])
 
     idx = np.argmin(np.abs(biases - 0.5))
