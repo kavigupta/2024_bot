132f3c6e0925a0c6a460e53e70ad296ebde11eee
Kavi Gupta <kavi@berkeley.edu>
2021-04-25 00:34:31 -0700

    rename variables

--- mapmaker/model.py
+++ mapmaker/model.py
@@ -138,18 +138,18 @@ class Model:
     def family_of_predictions(self, *, year, correct=True, n_seeds=1000):
         county_results, state_results, pop_votes = [], [], []
         for seed in range(n_seeds):
-            predictions, turnout_predictions = self.fully_random_sample(
+            predictions, turnout = self.fully_random_sample(
                 year=year, correct=correct, prediction_seed=seed
             )
             county_results.append(predictions)
             state_results.append(
                 get_state_results(
-                    self.metadata, dem_margin=predictions, turnout=turnout_predictions
+                    self.metadata, dem_margin=predictions, turnout=turnout
                 )
             )
             pop_votes.append(
                 get_popular_vote(
-                    self.metadata, dem_margin=predictions, turnout=turnout_predictions
+                    self.metadata, dem_margin=predictions, turnout=turnout
                 )
             )
         return np.array(county_results), np.array(state_results), np.array(pop_votes)
@@ -170,11 +170,11 @@ class Model:
             turnout_predictor.predict(features, correct, year=year),
         )
 
-    def win_consistent_with(self, predictions, turnout_predictions, seed):
+    def win_consistent_with(self, predictions, turnout, seed):
         if seed is None:
             return True
         dem, gop = get_electoral_vote(
-            self.metadata, dem_margin=predictions, turnout=turnout_predictions
+            self.metadata, dem_margin=predictions, turnout=turnout
         )
         dem_win = dem > gop  # ties go to gop
         # even days, democrat. odd days, gop
@@ -183,22 +183,22 @@ class Model:
     def sample(self, *, year, seed=None, correct=True):
         rng = np.random.RandomState(seed)
         while True:
-            predictions, turnout_predictions = self.fully_random_sample(
+            predictions, turnout = self.fully_random_sample(
                 year=year,
                 prediction_seed=rng.randint(2 ** 32) if seed is not None else None,
                 correct=correct,
             )
-            if self.win_consistent_with(predictions, turnout_predictions, seed):
+            if self.win_consistent_with(predictions, turnout, seed):
                 break
-        return predictions, turnout_predictions
+        return predictions, turnout
 
     def sample_map(self, title, path, **kwargs):
         print(f"Generating {title}")
-        predictions, turnout_predictions = self.sample(**kwargs)
+        predictions, turnout = self.sample(**kwargs)
         return generate_map(
             self.metadata,
             title,
             path,
             dem_margin=predictions,
-            turnout=turnout_predictions,
+            turnout=turnout,
         )
