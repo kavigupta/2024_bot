dfa63981319e360b5382dab2c16fcfc11ba82c04
Kavi Gupta <kavi@berkeley.edu>
2021-04-25 00:59:31 -0700

    undo hack

--- mapmaker/calibrator.py
+++ mapmaker/calibrator.py
@@ -12,7 +12,7 @@ def bias(preds):
 
 
 def calibrate(model, *, for_year, target_pv_spread_90=12.0e-2):
-    low_alpha, high_alpha = 0, 0.1016 * 2
+    low_alpha, high_alpha = 0, 1
     while True:
         mid_alpha = (low_alpha + high_alpha) / 2
         model = model.with_alpha(mid_alpha)
