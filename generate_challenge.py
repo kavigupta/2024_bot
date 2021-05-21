fa12f2ec8dee002db433ad404d8f37fc29586408
Kavi Gupta <kavi@berkeley.edu>
2021-05-21 01:00:04 -0700

    add challenge genertaor

---
+++ generate_challenge.py
@@ -0,0 +1,15 @@
+
+import sys
+import tqdm
+from mapmaker.challenge import generate_challenge_maps
+
+CHALLENGES = {
+    1: ("By Population", lambda x: x["CVAP"]),
+    2: ("Non-mainline prot", lambda x: x["evangelical"] + x["mormon"] + x["black_protestant"]),
+    3: ("2016 margin", lambda x: x["past_pres_partisanship"]),
+}
+
+challenges_to_generate = [int(x) for x in sys.argv[1:]]
+
+for i in tqdm.tqdm(challenges_to_generate):
+    generate_challenge_maps(i, *CHALLENGES[i])
