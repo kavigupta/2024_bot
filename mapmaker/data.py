ef4f63ffcb5b46c8161216f687d794ce555de1b5
Kavi Gupta <kavi@berkeley.edu>
2021-05-22 01:09:09 -0700

    add 4 reigons of alaska

--- mapmaker/data.py
+++ mapmaker/data.py
@@ -1,8 +1,8 @@
 import os
 from functools import lru_cache
 
-from urllib.request import urlopen
-import json
+import electiondata as e
+from electiondata.examples.plotly_geojson import PlotlyGeoJSON
 
 import numpy as np
 import pandas as pd
@@ -12,20 +12,65 @@ CSVS = os.path.join(os.path.dirname(__file__), "../csvs")
 
 @lru_cache(None)
 def counties():
-    with urlopen(
-        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
-    ) as response:
-        c = json.load(response)
-    [lakota] = [x for x in c["features"] if x["id"] == "46113"]
-    lakota["id"] = "46102"
-    return c
+    return PlotlyGeoJSON(e.alaska.FOUR_REGIONS()).get()
+
+
+def read_csv(path):
+    data = pd.read_csv(path, dtype=dict(FIPS=str))
+    data = data[[x for x in data if x != "Unnamed: 0"]]
+    data_ak = data[data.state == "Alaska"].copy()
+    data_not_ak = data[data.state != "Alaska"].copy()
+    normalizer = e.usa_county_to_fips("state", alaska_handler=e.alaska.FOUR_REGIONS())
+    normalizer.apply_to_df(data_ak, "county", "FIPS")
+    data_ak.dem_margin *= data_ak.CVAP
+    data_ak.past_pres_partisanship *= data_ak.CVAP
+
+    firsts = ["gisjoin", "county"]
+    sums = [
+        "CVAP",
+        "dem_margin",
+        "past_pres_partisanship",
+        "total_votes",
+    ]
+    means = [
+        "median_age",
+        "bachelor %",
+        "median_income",
+        "rural %",
+        "white %",
+        "black %",
+        "native %",
+        "asian %",
+        "hispanic %",
+        "evangelical",
+        "protestant",
+        "catholic",
+        "mormon",
+        "total_religious",
+        "black_protestant",
+        "poverty",
+    ]
+    agg = e.Aggregator(
+        grouped_columns=["FIPS"],
+        aggregation_functions={
+            **{k: lambda x: list(x)[0] for k in firsts},
+            **{k: np.sum for k in sums},
+            **{k: np.mean for k in means},
+        },
+    )
+    data_ak = agg(data_ak)
+
+    data_ak.dem_margin /= data_ak.CVAP
+    data_ak.past_pres_partisanship /= data_ak.CVAP
+    return pd.concat([data_ak, data_not_ak])
 
 
 def data_for_year(year):
-    data = pd.read_csv(
-        f"{CSVS}/election_demographic_data - {year}.csv", dtype=dict(FIPS=str)
+    data = read_csv(f"{CSVS}/election_demographic_data - {year}.csv")
+
+    data["FIPS"] = data["FIPS"].map(
+        lambda x: x if len(x) == 5 or x.startswith("02") else "0" + x
     )
-    data["FIPS"] = data["FIPS"].map(lambda x: x if len(x) == 5 else "0" + x)
     return data[
         [
             "FIPS",
@@ -67,7 +112,6 @@ def all_data(year):
         print("2012", data_2012["CVAP"].sum())
         print("2016", data_2016["CVAP"].sum())
         print("2020", all_data["CVAP"].sum())
-
         all_data["dem_margin"] = 0
         keys = [
             "median_age",
@@ -90,7 +134,6 @@ def all_data(year):
         # 2016 CVAP is centered in 2014
         # 2020 CVAP is centered in 2018
         # 2024 CVAP is centered in 2022
-
         # 2024 = 2020 + (2020 - 2016)
         # 2024 = 2020 + (2020 - 2012) / 2
         all_data["CVAP"] = (
@@ -98,7 +141,6 @@ def all_data(year):
             + ((all_data["CVAP"] - data_2016["CVAP"])) * 2.0 / 3
             + ((all_data["CVAP"] - data_2012["CVAP"]) / 2) * 1.0 / 3
         )
-
         all_data["CVAP"] = np.clip(all_data["CVAP"], 10, np.inf)
         print("2024", all_data["CVAP"].sum())
 
